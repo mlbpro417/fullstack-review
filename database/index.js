e23e8e122450d2bb6fcc284ddca660b70671bb9b
mlbpro417 <mlbpro417@gmail.com>
2018-02-16 15:18:11 -0800

    saving data to the databse

--- database/index.js
+++ database/index.js
@@ -1,16 +1,30 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  // repo id
+  id: Number,
+  // repo name
+  name: String,
+  // repo owner (username)
+  usernameOfOwner: String,
+  // repo url
+  url: String,
+  // repo number of forks
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
+let test = {id: 18221276, name: 'git-consortium', usernameOfOwner: 'octocat', url: "https://github.com/octocat/git-consortium", forks: 24}
 
+let save = (repo) => {
+  let newRepo = new Repo({id: repo.id, name: repo.name, usernameOfOwner: repo.usernameOfOwner, url: repo.url, forks: repo.forks});
+  newRepo.save();
+  // This function should save a repo or repos to
+  // the MongoDB 
+}
+save();
 module.exports.save = save;
